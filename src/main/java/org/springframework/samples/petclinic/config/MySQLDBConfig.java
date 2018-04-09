a027a6d9ac081fd5d5219fd004335e14342fb4b0
Unknown <jbk5988@gmail.com>
2018-04-09 13:22:07 -0400

    Added Second Configuration
    Added config for MYSQL and postgres

---
+++ src/main/java/org/springframework/samples/petclinic/config/MySQLDBConfig.java
@@ -0,0 +1,71 @@
+package org.springframework.samples.petclinic.config;
+
+import java.util.HashMap;
+
+    import javax.sql.DataSource;
+
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.context.annotation.Primary;
+    import org.springframework.context.annotation.PropertySource;
+    import org.springframework.core.env.Environment;
+    import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+    import org.springframework.jdbc.datasource.DriverManagerDataSource;
+    import org.springframework.orm.jpa.JpaTransactionManager;
+    import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+    import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+    import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@PropertySource({ "classpath:application.properties" })
+@EnableJpaRepositories(basePackages = "org.springframework.samples.petclinic", entityManagerFactoryRef = "userEntityManager", transactionManagerRef = "userTransactionManager")
+public class MySQLDBConfig {
+    @Autowired
+    private Environment env;
+
+    public MySQLDBConfig() {
+        super();
+    }
+
+    @Primary
+    @Bean
+    public LocalContainerEntityManagerFactoryBean userEntityManager() {
+        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(userDataSource());
+        em.setPackagesToScan(new String[] { "org.springframework.samples.petclinic" });
+
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        final HashMap<String, Object> properties = new HashMap<String, Object>();
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect", env.getProperty("hibernate.mysqldialect"));
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Primary
+    @Bean
+    public DataSource userDataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        dataSource.setSchema(env.getProperty("spring.datasource.schema"));
+
+        return dataSource;
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager userTransactionManager() {
+        final JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(userEntityManager().getObject());
+        return transactionManager;
+    }
+
+}
+
+
--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
+CREATE DATABASE petclinic;
 
 ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
@@ -8,58 +8,70 @@ GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
 
 USE petclinic;
 
+CREATE SEQUENCE vets_seq;
+
 CREATE TABLE IF NOT EXISTS vets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('vets_seq') PRIMARY KEY,
   first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX(last_name)
-) engine=InnoDB;
+  last_name VARCHAR(30)
+  CREATE INDEX(last_name)
+); engine=InnoDB;
+
+CREATE SEQUENCE specialties_seq;
 
 CREATE TABLE IF NOT EXISTS specialties (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
+  id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('specialties_seq') PRIMARY KEY,
+  name VARCHAR(80)
+  CREATE INDEX(name)
+); engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT(4) UNSIGNED NOT NULL,
-  specialty_id INT(4) UNSIGNED NOT NULL,
+  vet_id INT CHECK (vet_id > 0) NOT NULL,
+  specialty_id INT CHECK (specialty_id > 0) NOT NULL,
   FOREIGN KEY (vet_id) REFERENCES vets(id),
   FOREIGN KEY (specialty_id) REFERENCES specialties(id),
   UNIQUE (vet_id,specialty_id)
-) engine=InnoDB;
+); engine=InnoDB;
+
+CREATE SEQUENCE types_seq;
 
 CREATE TABLE IF NOT EXISTS types (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
+  id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('types_seq') PRIMARY KEY,
+  name VARCHAR(80)
+  CREATE INDEX(name)
+); engine=InnoDB;
+
+CREATE SEQUENCE owners_seq;
 
 CREATE TABLE IF NOT EXISTS owners (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('owners_seq') PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   address VARCHAR(255),
   city VARCHAR(80),
-  telephone VARCHAR(20),
-  INDEX(last_name)
-) engine=InnoDB;
+  telephone VARCHAR(20)
+  CREATE INDEX(last_name)
+); engine=InnoDB;
+
+CREATE SEQUENCE pets_seq;
 
 CREATE TABLE IF NOT EXISTS pets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('pets_seq') PRIMARY KEY,
   name VARCHAR(30),
   birth_date DATE,
-  type_id INT(4) UNSIGNED NOT NULL,
-  owner_id INT(4) UNSIGNED NOT NULL,
-  INDEX(name),
+  type_id INT CHECK (type_id > 0) NOT NULL,
+  owner_id INT CHECK (owner_id > 0) NOT NULL
+  CREATE INDEX(name),
   FOREIGN KEY (owner_id) REFERENCES owners(id),
   FOREIGN KEY (type_id) REFERENCES types(id)
-) engine=InnoDB;
+); engine=InnoDB;
+
+CREATE SEQUENCE visits_seq;
 
 CREATE TABLE IF NOT EXISTS visits (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  pet_id INT(4) UNSIGNED NOT NULL,
+  id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('visits_seq') PRIMARY KEY,
+  pet_id INT CHECK (pet_id > 0) NOT NULL,
   visit_date DATE,
   description VARCHAR(255),
   FOREIGN KEY (pet_id) REFERENCES pets(id)
-) engine=InnoDB;
+); engine=InnoDB;
