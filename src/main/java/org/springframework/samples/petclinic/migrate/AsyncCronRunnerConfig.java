c3e495a1a497eee7aa806bb86e5f4965bd5179ea
Unknown <jbk5988@gmail.com>
2018-04-10 19:02:44 -0400

    Implemented Async Runner and Consistency Check stub
    Added async cron running functionality and added Concsistency check stub

---
+++ src/main/java/org/springframework/samples/petclinic/migrate/AsyncCronRunnerConfig.java
@@ -0,0 +1,11 @@
+/*
+ * Software property of Acquisio. Copyright 2003-2018.
+ */
+package org.springframework.samples.petclinic.migrate;
+
+/**
+ * @author Gibran
+ */
+public class AsyncCronRunnerConfig {
+
+}
