239fc65563d961160d1d034a6f013164010cf77a
Unknown <jbk5988@gmail.com>
2018-04-10 16:34:31 -0400

    Rewiring Sources
    Rewired app to use new datastore and creted entitites and repos for old store

---
+++ src/main/java/org/springframework/samples/petclinic/oldDataStore/entities/PetOld.java
@@ -0,0 +1,11 @@
+/*
+ * Software property of Acquisio. Copyright 2003-2018.
+ */
+package org.springframework.samples.petclinic.oldDataStore.entities;
+
+/**
+ * @author Gibran
+ */
+public class PetOld {
+
+}
